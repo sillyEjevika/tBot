0bc653de43b8af0be514a0e840dec0c54afe7b88
zalex14 <9824018@gmail.com>
2023-04-07 12:57:07 +0300

    JS6_Team3

---
+++ src/main/java/js6team3/tbot/TBotApplication.java
@@ -0,0 +1,14 @@
+package js6team3.tbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TBotApplication.class, args);
+        System.out.println("JavaSprint6_Team3");
+    }
+
+}
---
+++ src/test/java/js6team3/tbot/TBotApplicationTests.java
@@ -0,0 +1,13 @@
+package js6team3.tbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
